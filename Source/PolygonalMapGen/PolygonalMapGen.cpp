d1e9102bc1bd72c4869fd042be9dfd914119b6c9
Jay2645 <disneylandjay@gmail.com>
2016-10-31 05:00:55 -0700

    Removed unneeded directories.

--- Source/PolygonalMapGen/PolygonalMapGen.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PolygonalMapGen.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PolygonalMapGen, "PolygonalMapGen" );
--- Source/PolygonalMapGen/PolygonalMapGenGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PolygonalMapGen.h"
-#include "PolygonalMapGenGameMode.h"
-
-
-
-
